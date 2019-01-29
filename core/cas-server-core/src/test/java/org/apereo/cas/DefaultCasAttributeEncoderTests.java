09121b8622fd148497f4dd1be0a9f5ee2af7964a
Timur Duehr <tduehr@gmail.com>
2019-01-29 12:57:46 -0600

    fix test

--- core/cas-server-core/src/test/java/org/apereo/cas/DefaultCasAttributeEncoderTests.java
+++ core/cas-server-core/src/test/java/org/apereo/cas/DefaultCasAttributeEncoderTests.java
@@ -5,7 +5,6 @@ import org.apereo.cas.services.RegisteredServiceTestUtils;
 import org.apereo.cas.services.ServicesManager;
 
 import lombok.val;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -25,8 +24,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 4.1
  */
 public class DefaultCasAttributeEncoderTests extends BaseCasCoreTests {
-    private Map<String, Object> attributes;
-
     @Autowired
     @Qualifier("servicesManager")
     private ServicesManager servicesManager;
@@ -35,36 +32,38 @@ public class DefaultCasAttributeEncoderTests extends BaseCasCoreTests {
         return Collections.singleton(attr);
     }
 
-    @BeforeEach
-    public void before() {
-        this.attributes = new HashMap<>();
-        IntStream.range(0, 3).forEach(i -> this.attributes.put("attr" + i, newSingleAttribute("value" + i)));
-        this.attributes.put(CasViewConstants.MODEL_ATTRIBUTE_NAME_PROXY_GRANTING_TICKET, newSingleAttribute("PGT-1234567"));
-        this.attributes.put(CasViewConstants.MODEL_ATTRIBUTE_NAME_PRINCIPAL_CREDENTIAL, newSingleAttribute("PrincipalPassword"));
-    }
-
     @Test
     public void checkNoPublicKeyDefined() {
+        val attributes = getAttributes();
         val service = RegisteredServiceTestUtils.getService("testDefault");
-        val encoder = new DefaultCasProtocolAttributeEncoder(this.servicesManager, CipherExecutor.noOpOfStringToString());
-        val encoded = encoder.encodeAttributes(this.attributes, this.servicesManager.findServiceBy(service));
-        assertEquals(this.attributes.size() - 2, encoded.size());
+        val encoder = new DefaultCasProtocolAttributeEncoder(servicesManager, CipherExecutor.noOpOfStringToString());
+        val encoded = encoder.encodeAttributes(attributes, servicesManager.findServiceBy(service));
+        assertEquals(attributes.size() - 2, encoded.size());
     }
 
     @Test
     public void checkAttributesEncodedCorrectly() {
+        val attributes = getAttributes();
         val service = RegisteredServiceTestUtils.getService("testencryption");
-        val encoder = new DefaultCasProtocolAttributeEncoder(this.servicesManager, CipherExecutor.noOpOfStringToString());
-        val encoded = encoder.encodeAttributes(this.attributes, this.servicesManager.findServiceBy(service));
-        assertEquals(encoded.size(), this.attributes.size());
-        checkEncryptedValues(CasViewConstants.MODEL_ATTRIBUTE_NAME_PRINCIPAL_CREDENTIAL, encoded);
-        checkEncryptedValues(CasViewConstants.MODEL_ATTRIBUTE_NAME_PROXY_GRANTING_TICKET, encoded);
+        val encoder = new DefaultCasProtocolAttributeEncoder(servicesManager, CipherExecutor.noOpOfStringToString());
+        val encoded = encoder.encodeAttributes(attributes, servicesManager.findServiceBy(service));
+        assertEquals(encoded.size(), attributes.size());
+        checkEncryptedValues(attributes, CasViewConstants.MODEL_ATTRIBUTE_NAME_PRINCIPAL_CREDENTIAL, encoded);
+        checkEncryptedValues(attributes, CasViewConstants.MODEL_ATTRIBUTE_NAME_PROXY_GRANTING_TICKET, encoded);
     }
 
-    private void checkEncryptedValues(final String name, final Map<String, Object> encoded) {
-        val v1 = ((Collection<?>) this.attributes.get(
+    private void checkEncryptedValues(final Map<String, Object> attributes, final String name, final Map<String, Object> encoded) {
+        val v1 = ((Collection<?>) attributes.get(
             name)).iterator().next().toString();
         val v2 = (String) encoded.get(name);
         assertNotEquals(v1, v2);
+    }
+
+    public Map<String, Object> getAttributes() {
+        val attributes = new HashMap<String, Object>();
+        IntStream.range(0, 3).forEach(i -> attributes.put("attr" + i, newSingleAttribute("value" + i)));
+        attributes.put(CasViewConstants.MODEL_ATTRIBUTE_NAME_PROXY_GRANTING_TICKET, newSingleAttribute("PGT-1234567"));
+        attributes.put(CasViewConstants.MODEL_ATTRIBUTE_NAME_PRINCIPAL_CREDENTIAL, newSingleAttribute("PrincipalPassword"));
+        return attributes;
     }
 }
